57ad01316cf27e6e831c37fc7fdcbc313e4698d9
GabbDup <gabriel.dupras01@gmail.com>
2025-02-21 12:42:35 -0500

    animation haut bas fonctionne

--- Assets/Script/SousmarinMouvement.cs
+++ Assets/Script/SousmarinMouvement.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class SousmarinMouvement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _vitesse;
+    private Rigidbody _rb;
+    private Vector3 directionInput;
+
+    [SerializeField] private float _modifierAnimTranslation;
+    private Animator _animator;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouvement(InputValue directionBase)
     {
-        
+        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesse;
+        directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
+    }
+
+    private void OnMouvementY(InputValue directionBase)
+    {
+        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesse;
+        directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
+    }
+    private void FixedUpdate()
+    {
+        Vector3 mouvement = directionInput;
+
+        _rb.AddForce(mouvement, ForceMode.VelocityChange);
+
+        Vector3 vitesseSurPlane = new Vector3(0f, _rb.velocity.y, _rb.velocity.x);
+        _animator.SetFloat("vitesse", vitesseSurPlane.magnitude * _modifierAnimTranslation);
+        _animator.SetFloat("Deplacement", vitesseSurPlane.magnitude);
     }
 }
