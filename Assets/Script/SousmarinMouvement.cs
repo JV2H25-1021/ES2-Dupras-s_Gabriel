edac5b6b29af87f62390b86aa2aa0e8806a36fd2
GabbDup <gabriel.dupras01@gmail.com>
2025-02-23 12:57:23 -0500

    modification des certaines condition et ajout de l'accélération sur appuie de la touche Shift

--- Assets/Script/SousmarinMouvement.cs
+++ Assets/Script/SousmarinMouvement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows;
@@ -25,19 +26,29 @@ public class SousmarinMouvement : MonoBehaviour
     
     }
 
-    private void OnAcceleration( )
+    private void OnAcceleration(InputValue context)
     {
-        
+        //Sur appuis de la touche "Shift" augmenter l'acceleration du Sousmarin
+        if (context.isPressed) 
+        {
+            _acceleration+=2;
+        }
+        else 
+        {
+            _acceleration--;
+        }        
     }
 
     private void OnMouvementY(InputValue directionBase)
     {
+        //Application des données recueillis pour faire monter ou descendre le Sousmarin
         Vector2 directionAvecVitesse = (directionBase.Get<Vector2>() * _vitesseY)*_acceleration;
         directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
     }
 
     private void OnMouvementZ(InputValue directionBase)
     {
+        //Application des données recueillis pour faire avancer ou reculer le Sousmarin
         Vector2 directionAvecVitesse = (directionBase.Get<Vector2>() * _vitesseX)*_acceleration;
         directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
 
@@ -45,14 +56,20 @@ public class SousmarinMouvement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Application de la force au Sousmarin
         Vector3 mouvement = directionInput;
-
         _rb.AddForce(mouvement, ForceMode.VelocityChange);
 
+        //Obtenir les données de force et les transférer en Vector3
         Vector3 vitesseSurPlane = new (0f, _rb.velocity.y, _rb.velocity.z);
-        _animator.SetFloat("VitesseX", vitesseSurPlane.z * _modifierAnimTranslation);
+        
+        //Gestion des animations
+
+        //Appliquer une transition sur les animations selon leurs axes 
+        _animator.SetFloat("VitesseZ", vitesseSurPlane.z * _modifierAnimTranslation);
+        _animator.SetFloat("DeplacementZ", vitesseSurPlane.z * _modifierAnimTranslation);
+
         _animator.SetFloat("VitesseY", vitesseSurPlane.y * _modifierAnimTranslation);
-        _animator.SetFloat("DeplacementX", vitesseSurPlane.z * _modifierAnimTranslation);
         _animator.SetFloat("AltitudeY", vitesseSurPlane.y * _modifierAnimTranslation);
 
     }
